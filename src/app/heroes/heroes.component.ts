bdd12419737f5fc31d8f6ea0069c651c8aaf68bd
Ethan Ware <ethan.ware@tcs.com>
2019-10-10 11:10:55 -0400

    Completed: 5. Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,15 +9,6 @@ import { HeroService } from '../hero.service'
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -26,4 +17,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
